fed3e94292a7e9e5e33f9619c0c54ca8ce47c129
ericsanto <ericjesus403@gmail.com>
2025-01-06 12:05:55 -0300

    feat: Criando package Main e criando classe Main

---
+++ src/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+import Aula02.Caneta;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Caneta pen = new Caneta(null, null, 0, 0, false);
+		
+		pen.writer("text");
+	}
+	
+	
+	
+
+}
